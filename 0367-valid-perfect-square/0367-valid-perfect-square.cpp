f587e2588c32782c7361926838fad93a09710d89
Hayat Zeynu <hayatzeynu576@gmail.com>
2025-10-19 22:47:15 +0300

    Time: 0 ms (100%), Space: 7.7 MB (53.82%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        if (num < 1) return false;
+
+        long long left = 1;
+        long long right = num;
+
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+            long long square = mid * mid;
+
+            if (square == num) {
+                return true;
+            } 
+            else if (square < num) {
+                left = mid + 1;
+            } 
+            else {
+                right = mid - 1; 
+            }
+        }
+        return false;
+    }
+};
