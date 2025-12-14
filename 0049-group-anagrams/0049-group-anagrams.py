a01aad3857f4fd0204b81a9a74cf94b611220381
Hayat Zeynu <hayatzeynu576@gmail.com>
2025-12-14 20:30:58 +0300

    Time: 8 ms (92.3%), Space: 20.7 MB (61.12%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,9 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = ''.join(sorted(word))
+            groups[key].append(word)
+
+        return list(groups.values())
