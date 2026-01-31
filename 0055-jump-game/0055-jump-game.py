319a3b3d6617a2cfe97382d05966a409ece32e80
Hayat Zeynu <hayatzeynu576@gmail.com>
2026-01-31 23:04:50 +0300

    Time: 21 ms (59.17%), Space: 20.2 MB (42.05%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canJump(self, nums):
+        maxReach = 0
+
+        for i in range(len(nums)):
+            if i > maxReach:
+                return False
+            maxReach = max(maxReach, i + nums[i])
+
+        return True
