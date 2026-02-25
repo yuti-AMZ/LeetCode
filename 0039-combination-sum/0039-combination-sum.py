1950863f27243bd5142c573bead2ddc1d063927d
Hayat Zeynu <hayatzeynu576@gmail.com>
2026-02-25 21:54:27 +0300

    Time: 7 ms (76.7%), Space: 19.9 MB (8.72%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,23 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        result = []
+
+        def backtrack(start, path, remaining):
+ 
+            if remaining == 0:
+                result.append(path[:])
+                return
+            
+            if remaining < 0:
+                return
+
+            for i in range(start, len(candidates)):
+ 
+                path.append(candidates[i])
+
+                backtrack(i, path, remaining - candidates[i])
+
+                path.pop()
+
+        backtrack(0, [], target)
+        return result
