517ca9b31fa14426ca56a69e7c75377b832f7f29
Hayat Zeynu <hayatzeynu576@gmail.com>
2026-03-08 19:43:29 +0300

    Time: 0 ms (100%), Space: 19.2 MB (84.61%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.py
@@ -0,0 +1,10 @@
+class Solution:
+    def heightChecker(self, heights):
+        expected = sorted(heights)
+        count = 0
+        
+        for i in range(len(heights)):
+            if heights[i] != expected[i]:
+                count += 1
+                
+        return count
