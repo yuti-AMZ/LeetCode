8cd8e2eeaec552aab7cea86b2d8ea938ec476617
Hayat Zeynu <hayatzeynu576@gmail.com>
2026-03-20 22:05:02 +0300

    Time: 0 ms (100%), Space: 19.2 MB (79.96%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,8 +1,10 @@
 class Solution:
     def removeElement(self, nums, val):
-        i = 0
-        for j in range(len(nums)):
-            if nums[j] != val:
-                nums[i] = nums[j]
-                i += 1
-        return i
+        k = 0  # position for next valid element
+        
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+                
+        return k
