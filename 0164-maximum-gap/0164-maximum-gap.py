6dc3774f468c82e20fb8960f627835a17ef119e1
Hayat Zeynu <hayatzeynu576@gmail.com>
2026-04-01 23:51:39 +0300

    Time: 137 ms (85.52%), Space: 31.9 MB (59.13%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.py
+++ 0164-maximum-gap/0164-maximum-gap.py
@@ -1,34 +1,15 @@
 class Solution:
-    def maximumGap(self, nums: list[int]) -> int:
+    def maximumGap(self, nums):
+      
         if len(nums) < 2:
             return 0
 
-        min_val, max_val = min(nums), max(nums)
-        n = len(nums)
-        gap = math.ceil((max_val - min_val) / (n - 1))
 
-        if gap == 0:  # all numbers are the same
-            return 0
-
-        # initialize buckets
-        buckets_min = [math.inf] * (n - 1)
-        buckets_max = [-math.inf] * (n - 1)
-
-        for num in nums:
-            if num == min_val or num == max_val:
-                continue
-            idx = (num - min_val) // gap
-            buckets_min[idx] = min(buckets_min[idx], num)
-            buckets_max[idx] = max(buckets_max[idx], num)
+        nums.sort()
 
         max_gap = 0
-        prev = min_val
+        for i in range(1, len(nums)):
+            gap = nums[i] - nums[i - 1]
+            max_gap = max(max_gap, gap)
 
-        for i in range(n - 1):
-            if buckets_min[i] == math.inf:
-                continue
-            max_gap = max(max_gap, buckets_min[i] - prev)
-            prev = buckets_max[i]
-
-        max_gap = max(max_gap, max_val - prev)
         return max_gap
