f3341a772cd8b317825491b0a379f658b8e3b993
Hayat Zeynu <hayatzeynu576@gmail.com>
2026-04-14 21:17:33 +0300

    Time: 0 ms (100%), Space: 19.2 MB (91.34%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,10 +2,12 @@ class Solution:
     def plusOne(self, digits):
         n = len(digits)
 
+        # Traverse from the end
         for i in range(n - 1, -1, -1):
             if digits[i] < 9:
                 digits[i] += 1
                 return digits
-            digits[i] = 0
+            digits[i] = 0  # carry
 
+        # If all digits were 9
         return [1] + digits
