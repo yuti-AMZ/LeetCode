20570d131345aa372d91da68e71a5711e8b1806d
Hayat Zeynu <hayatzeynu576@gmail.com>
2026-04-19 21:20:07 +0300

    Time: 2 ms (54.84%), Space: 19.4 MB (72.56%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -3,27 +3,27 @@ class Solution:
         result = []
 
         def backtrack(start, path):
-            # If we have 4 parts and used all characters
+            
             if len(path) == 4 and start == len(s):
                 result.append(".".join(path))
                 return
             
-            # Invalid case
+          
             if len(path) == 4 or start == len(s):
                 return
             
-            # Try 1 to 3 digits
+           
             for length in range(1, 4):
                 if start + length > len(s):
                     break
                 
                 segment = s[start:start + length]
                 
-                # Leading zero check
+               
                 if len(segment) > 1 and segment[0] == '0':
                     continue
                 
-                # Range check
+               
                 if int(segment) > 255:
                     continue
                 
