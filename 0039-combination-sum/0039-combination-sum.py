0fcfd52af9fdee4c676d3c44a0e06c45f6b2cdd7
Hayat Zeynu <hayatzeynu576@gmail.com>
2026-04-29 21:26:56 +0300

    Time: 7 ms (75.15%), Space: 19.5 MB (64.81%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,18 +3,15 @@ class Solution:
         result = []
 
         def backtrack(start, path, total):
-            # Base cases
             if total == target:
                 result.append(path[:])
                 return
             if total > target:
                 return
 
-            # Try all options starting from 'start'
             for i in range(start, len(candidates)):
                 path.append(candidates[i])
-                backtrack(i, path, total + candidates[i])  # reuse same element
-                path.pop()  # undo choice
-
+                backtrack(i, path, total + candidates[i])  
+                path.pop()
         backtrack(0, [], 0)
         return result
