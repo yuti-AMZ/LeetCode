ce9eb8116313be825db967769a6f57e304017b9d
Hayat Zeynu <hayatzeynu576@gmail.com>
2026-05-27 22:37:49 +0300

    Time: 471 ms (95.9%), Space: 19.6 MB (26.94%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,34 +1,30 @@
 class Solution:
-    def exist(self, board, word):
+    def exist(self, board: List[List[str]], word: str) -> bool:
+    
         rows, cols = len(board), len(board[0])
+        visited = set()
 
-        def dfs(r, c, i):
-            if i == len(word):
+        def dfs(r, c, k):
+            if k == len(word):
                 return True
 
-            if (
-                r < 0 or r >= rows or
-                c < 0 or c >= cols or
-                board[r][c] != word[i]
-            ):
+            if not (0 <= r < rows) or not (0 <= c < cols) or (r,c) in visited or board[r][c] != word[k]:
                 return False
-
-            temp = board[r][c]
-            board[r][c] = "#"  
-
-            found = (
-                dfs(r + 1, c, i + 1) or
-                dfs(r - 1, c, i + 1) or
-                dfs(r, c + 1, i + 1) or
-                dfs(r, c - 1, i + 1)
-            )
-
-            board[r][c] = temp  
-            return found
-
+            
+            visited.add((r,c))
+            res = dfs(r+1, c, k+1) or dfs(r-1, c, k+1) or dfs(r, c+1, k+1) or dfs(r, c-1, k+1)
+            visited.remove((r,c))
+            return res
+             
+        count = {}
+        for c in word:
+            count[c] = 1 + count.get(c, 0)
+        
+        if count[word[0]] > count[word[-1]]:
+            word = word[::-1]
+        
         for r in range(rows):
             for c in range(cols):
-                if dfs(r, c, 0):
-                    return True
-
+                if dfs(r, c, 0): return True
+        
         return False
